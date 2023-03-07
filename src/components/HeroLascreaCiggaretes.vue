8e3a553cd0e166ac43a0ce46dac45b4189b8cd2e
argentumCake <dhikarahmayadi@gmail.com>
2023-03-07 22:53:25 +0700

    *add-project*

---
+++ src/components/HeroLascreaCiggaretes.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- Hero Section Begin -->
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/img/hero-1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/img/hero-2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/img/hero-3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+    <!-- Hero Section End -->
+</template>
+
+<script>
+
+export default {
+    name: 'HeroLascreaCiggaretes',
+    components: {
+
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <header-lascrea-ciggaretes />
+    <hero-lascrea-ciggaretes />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderLascreaCiggaretes from '@/components/HeaderLascreaCiggaretes.vue'
+import HeroLascreaCiggaretes from '@/components/HeroLascreaCiggaretes.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderLascreaCiggaretes,
+    HeroLascreaCiggaretes,
+  }
+}
+</script>
