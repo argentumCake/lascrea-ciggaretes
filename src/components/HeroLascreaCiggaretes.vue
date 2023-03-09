117327770df1d370a5d11798ca3240a78ba1a867
argentumCake <dhikarahmayadi@gmail.com>
2023-03-09 19:55:27 +0700

    edit carousel

--- src/components/HeroLascreaCiggaretes.vue
+++ src/components/HeroLascreaCiggaretes.vue
@@ -1,31 +1,51 @@
 <template>
     <!-- Hero Section Begin -->
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/img/hero-1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/img/hero-2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/img/hero-3.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-</div>
+    <carousel :items-to-show="1">
+    <slide v-for="slide in 3" :key="slide">
+      <div class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg')">
+        <div class="container">
+          <div class="row banner-info">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+                <h1>Black friday</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                </p>
+                <a href="#" class="primary-btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </slide>
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
     <!-- Hero Section End -->
 </template>
 
 <script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 export default {
     name: 'HeroLascreaCiggaretes',
     components: {
-
+      Carousel,
+      Slide,
+      Pagination,
+      Navigation,
     },
 }
 </script>
 
-<style scoped>
-
+<style>
+  .set-bg{
+    width: 100%;
+    height: 500px;
+  }
+  .banner-info{
+    margin-top: 130px;
+  }
 </style>
